bd73b295e157df459f4028ca3c42d5f90dbec183
Jenish-1235 <jenishtogadiya549@gmail.com>
2025-07-27 18:43:21 +0530

    prog : implement ConnectionUtil and Main file for db connection.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,8 +10,49 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
+
+        System.out.println("Simulating Charity Donation Platform");
+
         Scanner sc = new Scanner(System.in);
-        Connection connection = ConnectionUtil.getConnection();
+        Connection conn = null;
+
+        try {
+            System.out.println("Choose : \n" +
+                    "1. Use local Database. (Will create schema and add dummy data locally.) \n" +
+                    "2. Use hostede Database. (Schema and dummy data already setup in cloud.) \n");
+            System.out.println("Enter your choice (1 or 2) : ");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter Database url: ");
+                    String url = sc.next();
+                    System.out.println("Enter Database username: ");
+                    String username = sc.next();
+                    System.out.println("Enter Database password: ");
+                    String password = sc.next();
+
+                    conn = ConnectionUtil.getLocalDbConnection(url, username, password);
+                    break;
+                case 2:
+                    System.out.println("Connecting to existing database... \n" +
+                            "info : This database already consists of schema created and dummy data inserted.");
+                    conn = ConnectionUtil.getHostedDbConnection();
+                    break;
+                default:
+                    System.out.println("Wrong choice. Exiting.");
+                    return;
+
+            }
+        }
+        catch (SQLException e){
+            System.out.println("Database connection error. Please try again.");
+        }
+
+    }
+
+    private static void simulateSystem(Scanner sc, Connection conn) {
+        System.out.println("CLI Implementation will go here.");
     }
 }
--- src/main/java/org/example/utils/ConnectionUtil.java
+++ src/main/java/org/example/utils/ConnectionUtil.java
@@ -8,25 +8,33 @@ import java.sql.Statement;
 
 public class ConnectionUtil {
 
-    public static Connection getConnection() throws SQLException {
-        try{
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "root", "root");
-            Statement stmt = conn.createStatement();
-            stmt.execute("CREATE DATABASE IF NOT EXISTS charity_sim");
-            stmt.close();
-            conn.close();
-        }catch(SQLException e){
-            System.out.println("Failed to create database");
-            throw e;
+    private static final String DEFAULT_DB_NAME = "charity_sim";
+    private static final String HOSTED_DB_URL = "jdbc:mysql://jenish.site:3306/" + DEFAULT_DB_NAME;
+    private static final String HOSTED_DB_USER = "root";
+    private static final String HOSTED_DB_PASS = "root";
+
+    public static Connection getLocalDbConnection(String url, String username, String password) throws SQLException {
+        try {
+            try (Connection localConn = DriverManager.getConnection(url, username, password);
+                 Statement statement = localConn.createStatement()) {
+                statement.execute("CREATE DATABASE IF NOT EXISTS " + DEFAULT_DB_NAME);
+                return DriverManager.getConnection(url + DEFAULT_DB_NAME, username, password);
+            }
         }
-        return getDatabaseConnection();
-    }
-    public static Connection getDatabaseConnection() throws SQLException {
-        try{
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/charity_sim", "root", "root");
-        }catch (SQLException e){
-            System.out.println("Failed to connect to database");
-            throw e;
+        catch (SQLException e) {
+            System.out.println("Local Database connection error. Falling back to hosted connection");
+            e.printStackTrace();
+            return getHostedDbConnection();
         }
     }
+
+    public static Connection getHostedDbConnection() throws SQLException {
+        try {
+            return DriverManager.getConnection(HOSTED_DB_URL, HOSTED_DB_USER, HOSTED_DB_PASS);
+        } catch (SQLException e) {
+            System.out.println("Failed to connect to hosted database.");
+            throw e;
+        }
+    }
+
 }
